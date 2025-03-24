97af08f794461d59b807649e874667ce0950655d
Ambiya12 <ambiyadms@gmail.com>
2025-03-24 15:39:25 +0100

    feat: add event routes and update API structure in index.js

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 import express from 'express';
 import 'dotenv/config';
 import connectDB from './database/client.js';
-import authRouter from './routes/auths.js';
+import authRouter from './routes/auth.js';
+import eventRouter from './routes/events.js';
 const PORT = process.env.PORT || 8000;
-
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', authRouter)
-
+app.use('/api', authRouter);
+app.use('/api', eventRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my event API');
 });
-
-
 
 connectDB();
 app.listen(8000, () => {
