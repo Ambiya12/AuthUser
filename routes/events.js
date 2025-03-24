37121142d65205903f821af9ccab09f44d4e223e
Ambiya12 <ambiyadms@gmail.com>
2025-03-24 15:39:33 +0100

    feat: add event router with user verification middleware

---
+++ routes/events.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { verifyUser } from "../middlewares/authMiddlewares.js";
+
+const eventRouter = Router(); 
+
+eventRouter.get('/events', verifyUser, (req, res) => {
+    res.send(`Welcome to my events`)
+})
+
+export default eventRouter;
