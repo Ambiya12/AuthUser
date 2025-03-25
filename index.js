55dfd6518490b0b170eefdd721ef83ba8765607b
Ambiya12 <ambiyadms@gmail.com>
2025-03-25 14:29:20 +0100

    feat: integrate service router and enable CORS in API

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import 'dotenv/config';
 import connectDB from './database/client.js';
 import authRouter from './routes/auth.js';
 import eventRouter from './routes/events.js';
+import cors from 'cors'
+import serviceRouter from './routes/service.js';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', authRouter);
-app.use('/api', eventRouter);
+app.use('/api', authRouter, eventRouter, serviceRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my event API');
