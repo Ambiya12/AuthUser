e030c6f055a2f43a3a80dfa3f5a1bba7ddde3cc5
Ambiya12 <ambiyadms@gmail.com>
2025-03-27 10:25:54 +0100

    feat: add userRouter to API routes for user management

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors'
 import connectDB from './database/client.js';
 import authRouter from './routes/auth.js';
 import eventRouter from './routes/events.js';
-import cors from 'cors'
 import serviceRouter from './routes/service.js';
+import userRouter from './routes/user.js';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', authRouter, eventRouter, serviceRouter);
+app.use('/api', authRouter, eventRouter, serviceRouter, userRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my event API');
