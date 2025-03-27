a0d57dea158ba6403895b07c497159f0e73babe7
Ambiya12 <ambiyadms@gmail.com>
2025-03-27 10:26:16 +0100

    feat: add update and delete routes for services in serviceRouter

--- routes/service.js
+++ routes/service.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
-import { createService, getAllServices } from '../controllers/serviceController.js'
+import { createService, getAllServices, updateService, deleteService } from '../controllers/serviceController.js'
 import { verifyUser } from '../middlewares/verifyUser.js'
 
 const serviceRouter = Router()
 
 serviceRouter.get('/service', getAllServices)
 serviceRouter.post('/service', verifyUser, createService)
+serviceRouter.put('/service/:id', verifyUser, updateService)
+serviceRouter.delete('/service/:id', verifyUser, deleteService)
 
 export default serviceRouter
