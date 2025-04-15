9753d3c5c3cd596162fc1b95e267ddbe08f626f8
Ambiya12 <ambiyadms@gmail.com>
2025-04-15 11:22:20 +0200

    feat: add image handling routes and static file serving

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ import authRouter from './routes/auth.js';
 import eventRouter from './routes/events.js';
 import serviceRouter from './routes/service.js';
 import userRouter from './routes/user.js';
+import fs from 'fs';
+import path from 'path';
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/images', express.static('public/images'));
 
 app.use('/api', authRouter, eventRouter, serviceRouter, userRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my event API');
+});
+
+app.get ('/public/images/:filename', (req, res) => {
+    const file = `public/images/${req.params.filename}`;
+    res.sendFile(path.resolve(file));
+});
+
+app.get('/images', (req, res) => {
+    fs.readdir('public/images', (err, files) => {
+        if (err) {
+            return res.status(500).send({ error: err});
+        }
+        res.send({ images: files });
+    });
 });
 
 connectDB();
