ba54dc73382ef036cf430e1f6dbff875fdf14ba2
Ambiya12 <ambiyadms@gmail.com>
2025-04-15 11:23:04 +0200

    feat: add image upload middleware to user registration route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import { createUser, loginUser } from '../controllers/authController.js';
-
+import { upload } from '../middlewares/uploadFile.js';
 
 const authRouter = Router();
 
-authRouter.post('/register', createUser)
+authRouter.post('/register', upload.single('image'), createUser)
 
 authRouter.post('/login', loginUser)
+
 
 export default authRouter;
